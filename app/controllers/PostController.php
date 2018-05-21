9ae25c1f4f6b7dec3d9d0a69308646904a58eda9
kotoshiroma <k_shiroma@funteam.co.jp>
2018-05-21 19:34:21 +0900

    不要ファイル削除、ブログ機能のための新規ファイル作成

---
+++ app/controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+class PostController extends ControllerBase {
+
+    public function indexAction() {
+
+    }
+
+    public function mypage_indexAction() {
+
+    }
+
+    public function addAction() {
+        
+    }
+
+    public function editAction() {
+        
+    }
+}
